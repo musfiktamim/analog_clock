f5473952c89fe0e9c253c8ffc4d7a404d373da93
musfik tamim <musfiktamim@gmail.com>
2024-10-15 20:24:00 +0600

    some changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const sec_hand = document.querySelector(".sec-hand")
 const dh = document.querySelector(".dh")
 const dm = document.querySelector(".dm")
 const ds = document.querySelector(".ds")
+const tmss = document.querySelector(".timess")
 
 setInterval(()=>{
     const times = new Date();
@@ -13,11 +14,12 @@ setInterval(()=>{
     let s = times.getSeconds();
     let hrotation = 30*h+h/2;
     let mrotation = 6*m;
-    let srotation = 3*s;
+    let srotation = 3*s*2;
 
-    dh.innerText = h;
+    dh.innerText = h>12?h-13+1:h;
     dm.innerText = m;
     ds.innerText = s;
+    tmss.innerText = h>11?"PM":"AM"
 
     hour_hand.style.transform = `rotate(${hrotation}deg)`
     min_hand.style.transform = `rotate(${mrotation}deg)`
